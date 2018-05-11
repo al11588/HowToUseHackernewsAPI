c34d90d771094f6db63ec035405c36d38aeec42a
Alvin Lawson <al11588@gmail.com>
2018-05-10 22:45:07 -0400

    fixing stuff

--- hackernews.py
+++ hackernews.py
@@ -7,14 +7,14 @@ def hackernewsgetapirequest():
 	req = requests.get('https://hacker-news.firebaseio.com/v0/updates.json?print=pretty')
 	print (req.text)
 	print ("Status Code", req.status_code)
-	
-# Indicates which techniques are supported
+
+# Indicates which techniques are supported.
 def hackernewsoptionsapirequest():
 	req = requests.options('https://hacker-news.firebaseio.com/v0/updates.json?print=pretty')
 	print (req.text)
 	print ("Status Code", req.status_code)
 
-# It returns meta information. 
+# Returns meta information in that given URL. 
 def hackernewsheadapirequest():
 	req = requests.head('https://hacker-news.firebaseio.com/v0/updates.json?print=pretty')
 	print (req.text)
